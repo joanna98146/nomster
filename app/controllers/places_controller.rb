a037089e0bd7c1a28305cce48823aade81c2f034
Joanna Tracy <joanna98146@yahoo.com>
2017-08-24 13:39:49 +0000

    restricted name field so it cant be blank, added error message, and styled it

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     
      def index
-           @places = Place.paginate(:page => 3, :per_page => 5)
+        @places = Place.paginate(page: params[:page], :per_page => 5)
      end
      
      def new
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
      end
      
      def create
-    current_user.places.create(place_params)
-    redirect_to root_path 
+     @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
      end
      
   def show
@@ -31,8 +35,13 @@ class PlacesController < ApplicationController
    if @place.user != current_user
     return render text: 'Not Allowed', status: :forbidden
    end
+   
    @place.update_attributes(place_params)
-   redirect_to root_path
+   if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+   end
    end
    
    def destroy
