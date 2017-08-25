684b5344371e3bc48f0d0f77bcde18d882fc56d0
Joanna Tracy <joanna98146@yahoo.com>
2017-08-25 15:14:41 +0000

    added valdiations for address, description, and added name length requirements

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#
+# To Run:  $ rails db:seed
+# 
+
+# Reset database
+Place.destroy_all
+
+# Add places here
+#Place.create!(name: ????, address: ???, description: ???)
