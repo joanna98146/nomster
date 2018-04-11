64bf97f77275bdb950eb0cc39d879e2821b13e57
Joanna Tracy <joanna98146@yahoo.com>
2018-04-11 03:04:14 +0000

    did a bundle update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  Rails.application.routes.draw do
   devise_for :users
     root 'places#index'
     
@@ -7,5 +8,7 @@ Rails.application.routes.draw do
     resources :photos, only: :create 
     end
     resources :users, only: :show
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
